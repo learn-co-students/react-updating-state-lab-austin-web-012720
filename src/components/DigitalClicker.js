3a0a5438faccccbd585b4f25f23fbdefc47ac26b
lughjw <lughjw@gmail.com>
2020-03-31 21:06:49 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
+import React from "react";
+
 // Code DigitalClicker Component Here
+export default class DigitalClicker extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0,
+        }
+    }
+
+    render() {
+        return <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+
+    }
+
+    handleClick = () => {
+        this.setState({
+            timesClicked: this.state.timesClicked + 1
+        })
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React from "react";
+
+export default class YouTubeDebugger extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <button className='bitrate' onClick={this.handleChangeBitrate}>bitrate</button>
+                <button className='resolution' onClick={this.handleChangeResolution}>resolution</button>
+            </div>
+        )
+    }
+
+    handleChangeBitrate = () => {
+        this.setState({
+          settings: {
+              // Important to spread the settings so that we don't lose any of them
+            ...this.state.settings,
+            bitrate: 12
+          }
+        });
+      };
+    
+      handleChangeResolution = () => {
+        this.setState({
+          settings: {
+            // Important to spread the settings so that we don't lose any of them
+            ...this.state.settings,
+            video: {
+              ...this.state.settings.video,
+              resolution: '720p'
+            }
+          }
+        });
+    }
+
+    componentDidUpdate = () => {
+        console.log(this.state)
+    }
+}
