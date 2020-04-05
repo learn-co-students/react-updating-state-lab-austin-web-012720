f98a4b70556d67c2a61f9680f9ec9bc815ca351f
mattjagiello <mjagiello@gmail.com>
2020-04-05 15:00:50 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            timesClicked : 0
+        };
+    }
+
+    handleClick = () => {
+        let numClicked = this.state.timesClicked
+        this.setState({
+            timesClicked : numClicked + 1
+        })
+    };
+    
+    render() {
+        return (
+            <div>
+                {/* <p>Clicked {this.state.timesClicked} times</p> */}
+                <button onClick = { this.handleClick }>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,58 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        };
+    }
+
+    changeBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    };
+
+    changeResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+            }
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <p> Current bitrate: {this.state.settings.bitrate} </p>
+                <button
+                    className="bitrate"
+                    onClick={this.changeBitrate}> Change bitrate to 12
+                </button>
+                <p> Current video resolution: {this.state.settings.video.resolution} </p>
+                <button
+                    className="resolution"
+                    onClick={this.changeResolution}> Change resolution to 720p
+                </button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
